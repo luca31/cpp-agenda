f21484ad2e15895e9344b3f5b5d6956408b9960d
Laura Saccani <37377750+laurasac@users.noreply.github.com>
2019-11-05 09:39:09 +0100

    Update contact.cpp

--- contact.cpp
+++ contact.cpp
@@ -2,7 +2,7 @@
 
 using namespace std;
 
-Contact::Contact(string newName, string newLname, long long int newNumber, string newAddress="" ,string newEmail=""){
+Contact::Contact(string newName, string newLname,  newNumber, string newAddress="" ,string newEmail=""){
     name = newName;
     lname = newLname;
     number = newNumber;
